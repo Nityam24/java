7f33d3de25230df828986675762512568502fd31
Nityam24 <nityampatel2402@gmail.com>
2023-03-24 12:06:33 +0530

    first java commit

---
+++ innerClass.java
@@ -0,0 +1,26 @@
+class CPU {
+    double price;
+    class Processor{ // nested class
+    double cores;
+    double catch();
+    String manufacturer;
+    
+    
+    double getCache();
+    void displayProcesorDetail();
+    }
+    protected class RAM{ // nested protected class
+    // members of protected nested class
+    double memory;
+    String manufacturer;
+    Double clockSpeed;
+    double getClockSpeed();
+    void displayRAMDetail();
+    }
+    }
+
+    class Demo{
+        public static void main(String arg[]){
+
+        }
+    }
